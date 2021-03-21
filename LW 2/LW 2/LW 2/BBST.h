1111e28329124aa2f6b944b1e8505dde47b770f8
IgorMich2 <70755229+IgorMich2@users.noreply.github.com>
2021-03-21 09:47:46 +0200

    Create files

---
+++ LW 2/LW 2/LW 2/BBST.h
@@ -0,0 +1,11 @@
+#pragma once
+#ifndef _BBST_H
+#define _BBST_H
+
+#include<iostream>
+#include<string>
+using namespace std;
+
+
+
+#endif
---
+++ LW 2/LW 2/LW 2/Source.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<string>
+#include "BBST.h"
+using namespace std;
+
+
+int main()
+{
+    cout << "100"<<endl;
+
+    return 0;
+}
